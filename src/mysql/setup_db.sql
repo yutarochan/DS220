7c604f05fb2197643594322db42cd00b6ded3546
Yuya Jeremy Ong <yuyajeremyong@gmail.com>
2018-11-02 14:02:42 -0400

    (fix) Use default database

--- src/mysql/setup_db.sql
+++ src/mysql/setup_db.sql
@@ -5,9 +5,8 @@ Author: Yuya Jeremy Ong (yjo5006@psu.edu)
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
-/* Create and Use Database */
-CREATE DATABASE stackoverflow;
-USE stackoverflow;
+/* Use Provided Database */
+USE ds497;
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
