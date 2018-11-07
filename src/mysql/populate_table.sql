de26682c800d71bc523b5e0c39affa520e8e0995
Yuya Jeremy Ong <yuyajeremyong@gmail.com>
2018-11-07 15:46:25 -0500

    (fix) Fix data population table issue with columns

--- src/mysql/populate_table.sql
+++ src/mysql/populate_table.sql
@@ -5,18 +5,18 @@ Author: Yuya Jeremy Ong (yjo5006@psu.edu)
 
 /* Load Comment to Answer Table */
 LOAD DATA INFILE '../../data/csv/sx-stackoverflow-c2a.csv'
-INTO TABLE c2a
+INTO TABLE c2a (@dummy, source, target, timestamp)
 COLUMNS TERMINATED BY ','
 LINES TERMINATED BY '\n';
 
 /* Load Comment to Question Table */
 LOAD DATA INFILE '../../data/csv/sx-stackoverflow-c2q.csv'
-INTO TABLE c2q
+INTO TABLE c2q (@dummy, source, target, timestamp)
 COLUMNS TERMINATED BY ','
 LINES TERMINATED BY '\n';
 
 /* Load Answer to Question Table */
 LOAD DATA INFILE '../../data/csv/sx-stackoverflow-a2q.csv'
-INTO TABLE a2q
+INTO TABLE a2q (@dummy, source, target, timestamp)
 COLUMNS TERMINATED BY ','
 LINES TERMINATED BY '\n';
