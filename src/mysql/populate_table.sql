4857058d877a4ec90af71b6e8834c576e137d223
Yuya Jeremy Ong <yuyajeremyong@gmail.com>
2018-11-07 15:22:43 -0500

    (update) Update file to use for data population

--- src/mysql/populate_table.sql
+++ src/mysql/populate_table.sql
@@ -4,19 +4,19 @@ Author: Yuya Jeremy Ong (yjo5006@psu.edu)
 */
 
 /* Load Comment to Answer Table */
-LOAD DATA INFILE '../../data/raw/sx-stackoverflow-c2a.txt'
+LOAD DATA INFILE '../../data/csv/sx-stackoverflow-c2a.csv'
 INTO TABLE c2a
 COLUMNS TERMINATED BY ','
 LINES TERMINATED BY '\n';
 
 /* Load Comment to Question Table */
-LOAD DATA INFILE '../../data/raw/sx-stackoverflow-c2q.txt'
+LOAD DATA INFILE '../../data/csv/sx-stackoverflow-c2q.csv'
 INTO TABLE c2q
 COLUMNS TERMINATED BY ','
 LINES TERMINATED BY '\n';
 
 /* Load Answer to Question Table */
-LOAD DATA INFILE '../../data/raw/sx-stackoverflow-a2q.txt'
+LOAD DATA INFILE '../../data/csv/sx-stackoverflow-a2q.csv'
 INTO TABLE a2q
 COLUMNS TERMINATED BY ','
 LINES TERMINATED BY '\n';
