2ea98ec9fbfec8ec7956a8d1ed552ed05a23ba9e
Yuya Jeremy Ong <yuyajeremyong@gmail.com>
2018-11-07 16:49:34 -0500

    (update) Fix SQL data population import for new format

--- src/mysql/populate_table.sql
+++ src/mysql/populate_table.sql
@@ -5,18 +5,18 @@ Author: Yuya Jeremy Ong (yjo5006@psu.edu)
 
 /* Load Comment to Answer Table */
 LOAD DATA LOCAL INFILE '../../data/csv/sx-stackoverflow-c2a.csv'
-INTO TABLE c2a (source, target, timestamp)
+INTO TABLE c2a
 COLUMNS TERMINATED BY ','
 LINES TERMINATED BY '\n';
 
 /* Load Comment to Question Table */
 LOAD DATA LOCAL INFILE '../../data/csv/sx-stackoverflow-c2q.csv'
-INTO TABLE c2q (source, target, timestamp)
+INTO TABLE c2q
 COLUMNS TERMINATED BY ','
 LINES TERMINATED BY '\n';
 
 /* Load Answer to Question Table */
 LOAD DATA LOCAL INFILE '../../data/csv/sx-stackoverflow-a2q.csv'
-INTO TABLE a2q (source, target, timestamp)
+INTO TABLE a2q
 COLUMNS TERMINATED BY ','
 LINES TERMINATED BY '\n';
