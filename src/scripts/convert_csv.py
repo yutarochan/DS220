222bc2b7f778a050d57b8e73610a91101995039e
Yuya Jeremy Ong <yuyajeremyong@gmail.com>
2018-11-07 15:01:34 -0500

    (update) Complete CSV file converter script

--- src/scripts/convert_csv.py
+++ src/scripts/convert_csv.py
@@ -5,6 +5,24 @@ Author: Yuya Jeremy Ong (yjo5006@psu.edu)
 from __future__ import print_function
 import argparse
 
-def parse_arg():
+def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--')
+    parser.add_argument("--data", type=str, required=True, help="Original dataset to convert to csv")
+    parser.add_argument("--out", type=str, required=True, help="Output Directory")
+    return parser.parse_args()
+
+if __name__ == '__main__':
+    # Parse Arguments
+    args = parse_args()
+
+    # Open Load File
+    data = open(args.data, 'r').split('\n')[:-1]
+
+    # Write Output CSV File
+    out = open(args.out + '/' + args.data.split('.')[0] + '.csv', 'w')
+    for row in data:
+        r = row.split(' ')
+        out.write(','.join(r) + '\n')
+    out.close()
+
+    print('> DONE!')
